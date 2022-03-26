d8f3de2a03e22fed90a07677370734743cc45afd
David <popadavid843@yahoo.com>
2022-03-26 21:47:13 +0200

    modal created

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const closeModal = document.querySelector(".close");
+const openModal = document.querySelectorAll(".open-modal");
+const modalContainer = document.getElementById("modal-container");
+
 const dateToday = function () {
   let today = new Date();
   let date =
@@ -5,3 +9,12 @@ const dateToday = function () {
   document.getElementById("counter").innerHTML = date;
 };
 dateToday();
+
+closeModal.addEventListener("click", () => {
+  modalContainer.classList.add("active");
+});
+openModal.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    modalContainer.classList.remove("active");
+  });
+});
